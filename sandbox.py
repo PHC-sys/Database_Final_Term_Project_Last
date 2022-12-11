6cf50037e4623428672c8546eaf0095312c935b8
PHC-sys <94788938+PHC-sys@users.noreply.github.com>
2022-12-12 01:35:04 +0900

    Delete sandbox.py

--- sandbox.py
+++
@@ -1,9 +0,0 @@
-def converter(*args):
-    length = len(args)
-    string = ''
-    for i in range(length):
-        string += args[i]
-        if length-i == 1:
-            break
-        string += ','
-    return string
